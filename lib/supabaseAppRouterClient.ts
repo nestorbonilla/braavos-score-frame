5dfa1471b105f6e09254a236a6fd0a8c949ed7f8
Nestor Bonilla <nestor.bonilla.s@gmail.com>
2024-02-10 12:48:06 -0800

    making .env vars private

--- lib/supabaseAppRouterClient.ts
+++ lib/supabaseAppRouterClient.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 
 export function createSupabaseAppServerClient(serverComponent = false) {
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
       cookies: {
