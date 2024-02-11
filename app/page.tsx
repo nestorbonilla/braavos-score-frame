007520cd4a326b2abedfd001ef690163348657bc
Nestor Bonilla <nestor.bonilla.s@gmail.com>
2024-02-11 13:31:29 -0800

    updating web home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
-import { getFrameMessage } from "frames.js";
-import {
-  FrameButton,
-  FrameContainer,
-  FrameImage,
-  NextServerPageProps,
-  getPreviousFrame,
-} from "frames.js/next/server";
 
 let imageUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/api/images`;
 
@@ -25,7 +17,6 @@ let frameMetadata = getFrameMetadata({
   ],
   image: imageUrl,
   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/redirect`,
-  // post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame`,
 });
 
 export const metadata: Metadata = {
@@ -43,8 +34,25 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <>
-      <h1>Braavos ProScore Leaderboard</h1>
-    </>
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-3xl">
+        <div className="flex justify-center items-center min-h-screen">
+          <div className="text-center">
+            <div>
+              <h1 className="text-2xl mdx:text-3xl lg:text-4xl font-bold leading-tight text-white dark:text-gray-200">Braavos Pro Score: Top Players</h1>
+              <p className="mt-4 text-md md:text-lg lg:text-xl text-gray-300 dark:text-gray-400 leading-normal py-2 px-4 dark:bg-gray-800 rounded-lg">
+                Welcome, Braavos Pro!
+              </p>
+              <p className="mt-4 text-md md:text-lg lg:text-xl text-gray-300 dark:text-gray-400 leading-normal py-2 px-4 dark:bg-gray-800 rounded-lg">
+                Join the elite of Braavos wallet users in our daily leaderboard challenge! Connect, verify, and play to see your name among the top 10. Are you ready to claim your spot?
+              </p>
+              <p className="mt-4 text-md md:text-lg lg:text-sm text-gray-300 dark:text-gray-400 leading-normal py-2 px-4 dark:bg-gray-800 rounded-lg">
+                * Heads up! This app is exclusively for Braavos wallets on Starknet Mainnet. Update your score every 24 hours directly through our Farcaster frame.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
